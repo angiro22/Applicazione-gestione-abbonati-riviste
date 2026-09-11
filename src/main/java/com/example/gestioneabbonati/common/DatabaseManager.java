18f04ef5c4ac62e5d7022e4a8e6834c7c4873461
angiro22 <andreagirone2710@gmail.com>
2026-09-11 15:16:36 +0200

    feat: add subscriber method created

--- src/main/java/com/example/gestioneabbonati/common/DatabaseManager.java
+++ src/main/java/com/example/gestioneabbonati/common/DatabaseManager.java
@@ -1,18 +1,46 @@
 package com.example.gestioneabbonati.common;
 
+import com.example.gestioneabbonati.Subscriber;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseManager {
         private static final String DB_PATH = "database/abbonati.db"; // dir database
         private static final String URL = "jdbc:sqlite:" + DB_PATH; // url connessione SQLite
         private static final String SCHEMA_PATH = "abbonati/schema.sql"; // dir istruzioni sql ddl
+
+        // Inserire abbonati nel db
+        public static boolean addSubscriber(Subscriber subscriber) {
+                // I '?' verranno sostituiti con i dati dell'abbonato
+                String sql = """
+                        INSERT INTO abbonati (code, magazineName, secondName, name, address, gender, city)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)
+                        """;
+                try (Connection conn = getConnection()) {
+                        PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+                                // Sostituzione dei '?' con i dati dell'abbonato
+                                preparedStatement.setString(1, subscriber.getCode());
+                                preparedStatement.setString(2, subscriber.getMagazineName());
+                                preparedStatement.setString(3, subscriber.getSecondName());
+                                preparedStatement.setString(4, subscriber.getName());
+                                preparedStatement.setString(5, subscriber.getAddress());
+                                preparedStatement.setString(6, subscriber.getGender());
+                                preparedStatement.setString(7, subscriber.getCity());
+
+                                // Inserimento nel database
+                                int rowsAffected = preparedStatement.executeUpdate();
+                                return rowsAffected > 0;
+                        }
+                } catch (SQLException e) {
+                        System.err.println("Errore durante l'inserimento dell'abbonato: " + e.getMessage());
+                        e.printStackTrace();
+                        return false;
+                }
+        }
 
         // Connessione al database
         public static Connection getConnection() throws SQLException {
